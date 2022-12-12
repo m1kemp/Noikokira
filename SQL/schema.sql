775f745fbd58196c6849a06cb7d6182669ce4806
GeorgeMos <georgemosxonas13@gmail.com>
2022-12-12 21:09:37 +0200

    update

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE item_category(
 );
 
 CREATE TABLE item(
-    item_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    item_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
     category_id SMALLINT UNSIGNED NOT NULL,
     item_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (item_id, category_id),
@@ -39,9 +39,22 @@ CREATE TABLE offer(
     offer_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     likes SMALLINT NOT NULL,
     dislikes SMALLINT NOT NULL,
-    item_id SMALLINT UNSIGNED NOT NULL,
+    item_id MEDIUMINT UNSIGNED NOT NULL,
     user_id SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY(offer_id),
     CONSTRAINT fk_offer_item FOREIGN KEY (item_id) REFERENCES item(item_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_offer_user FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE RESTRICT ON UPDATE RESTRICT #Offer cant be transfered!
-)
+);
+
+CREATE TABLE store(
+    store_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    store_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY(store_id)
+);
+
+CREATE TABLE store_inventory(
+    inventory_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    store_id MEDIUMINT UNSIGNED NOT NULL,
+    item_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (inventory_id)
+);
