5c37bcdad6bde66ba8a1280fdd4df1fbff983863
GeorgeMos <57734043+GeorgeMos@users.noreply.github.com>
2022-12-30 18:57:25 +0200

    Removed NOT NULL Constraint

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE user(
     first_name VARCHAR(45) NOT NULL,
     last_name VARCHAR(45) NOT NULL ,
     email VARCHAR(45) NOT NULL ,
-    address_id SMALLINT UNSIGNED NOT NULL ,
+    address_id SMALLINT UNSIGNED ,
     points SMALLINT NOT NULL ,
     tokens SMALLINT NOT NULL ,
     password VARCHAR(64) NOT NULL,
