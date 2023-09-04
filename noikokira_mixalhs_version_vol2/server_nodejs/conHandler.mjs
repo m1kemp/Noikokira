c124c0c8cec7a7108df203322bb983fb304053a6
m1kemp <mixalhs0mpallas@gmail.com>
2023-09-04 20:00:32 +0300

    editProfle

---
+++ noikokira_mixalhs_version_vol2/server_nodejs/conHandler.mjs
@@ -0,0 +1,172 @@
+import mysql from "mysql"
+import {con} from "./test.mjs"
+import fs from "fs"
+
+ function sendQuery(querySend){
+     con.query(querySend,(err,result,fields)=>{
+        if (err){
+            return console.log(err);
+        }
+        else return result;
+    })
+ }
+
+
+
+function updateSupermarkets(fileName) {
+    var filePath = "./uploads/" + fileName;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var supermarketData = JSON.parse(data);
+        var superArray = supermarketData.elements;
+        
+        // Prepare an array to hold values for bulk insert
+        const valuesArray = [];
+
+        for (var i = 0; i < superArray.length; i++) {
+            var name = superArray[i].tags.name;
+            var lat = superArray[i].lat;
+            var lon = superArray[i].lon;
+
+            if (typeof name !== "undefined") {
+                // Push values into the array for bulk insert
+                valuesArray.push([name, lat, lon]);
+            }
+        }
+
+        // Bulk insert using a single query
+        if (valuesArray.length > 0) {
+            const query = "INSERT INTO STORE (store_name, store_lat, store_lon) VALUES ?";
+            con.query(query, [valuesArray], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("Inserted rows: " + result.affectedRows);
+                }
+            });
+        }
+    });
+}
+
+function removeSupermarkets(fileName) {
+    var filePath = "./uploads/" + fileName;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var supermarketData = JSON.parse(data);
+        var superArray = supermarketData.elements;
+        
+        // Prepare an array to hold values for bulk insert
+        const valuesArray = [];
+
+        for (var i = 0; i < superArray.length; i++) {
+            var name = superArray[i].tags.name;
+            var lat = superArray[i].lat;
+            var lon = superArray[i].lon;
+
+            if (typeof name !== "undefined") {
+                // Push values into the array for bulk insert
+                valuesArray.push([name, lat, lon]);
+            }
+        }
+
+        // Bulk insert using a single query
+        if (valuesArray.length > 0) {
+            const query = "DELETE FROM STORE WHERE (store_name,store_lat, store_lon) IN (?)";
+            con.query(query, [valuesArray], (err, result) => {
+                if (err) {
+                    console.log("db error");
+                    console.error(err);
+                } else {
+                    console.log("Deleted rows: " + result.affectedRows);
+                }
+            });
+        }
+    });
+}
+
+function updateProducts(fileName) {
+    var filePath = "./uploads/" + fileName;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var prodData = JSON.parse(data);
+        var prodArray = prodData.products;
+        
+        // Prepare an array to hold values for bulk insert
+        const valuesArray = [];
+
+        for (var i = 0; i < prodArray.length; i++) {
+            var name = prodArray[i].name;
+
+            if (typeof name !== "undefined") {
+                // Push values into the array for bulk insert
+                valuesArray.push([name]);
+            }
+        }
+        
+        //TODO:Add a query for adding a category for the inserted items 
+        //sendQuery()
+
+        // Bulk insert using a single query
+        if (valuesArray.length > 0) {
+            const query = "INSERT INTO item (item_name) VALUES ?";
+            con.query(query, [valuesArray], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("Inserted rows: " + result.affectedRows);
+                }
+            });
+        }
+    });
+}
+
+function removeProducts(fileName) {
+    var filePath = "./uploads/" + fileName;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var prodData = JSON.parse(data);
+        var prodArray = prodData.products;
+        
+        // Prepare an array to hold values for bulk insert
+        const valuesArray = [];
+
+        for (var i = 0; i < prodArray.length; i++) {
+            var name = prodArray[i].name;
+
+            if (typeof name !== "undefined") {
+                // Push values into the array for bulk insert
+                valuesArray.push([name]);
+            }
+        }
+
+        // Bulk insert using a single query
+        if (valuesArray.length > 0) {
+            const query = "DELETE FROM item WHERE (item_name) IN (?)";
+            con.query(query, [valuesArray], (err, result) => {
+                if (err) {
+                    console.log("db error");
+                    console.error(err);
+                } else {
+                    console.log("Deleted rows: " + result.affectedRows);
+                }
+            });
+        }
+    });
+}
+export { sendQuery, updateSupermarkets,removeSupermarkets, updateProducts, removeProducts };
---
+++ noikokira_mixalhs_version_vol2/server_nodejs/router.mjs
@@ -0,0 +1,212 @@
+import express from "express"
+import mysql from "mysql"
+import {con} from "./test.mjs"
+import {sendQuery, updateSupermarkets, removeSupermarkets, updateProducts, removeProducts} from "./conHandler.mjs"
+
+import fileUpload from "express-fileupload";
+import cors from "cors"
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import _ from "lodash";
+
+const router=express.Router()
+
+
+router.get("/",(req,res)=>{
+    res.render("admin_user")
+})
+
+router.get("/login_admin",(req,res)=>{
+    res.render("login_admin")
+})
+router.post("/homepage_admin",(req,res)=>{
+    const {username,password}=req.body;
+    con.query("SELECT email from admin WHERE username=? and password=?",
+    [username,password],(error,result)=>{
+        if(error){
+            console.log(error);
+        }
+        else if(result.length>0){
+    
+    return res.render("homepage_admin",{username:username})
+        }
+        else{
+            res.render("login_admin",{message:"The account does not exist"})
+        }
+        })
+        })
+
+router.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+router.get("/validation",(req,res)=>{
+  res.render("validation");
+})
+
+router.post("/editProfile",(req,res)=>{
+  const{username,password}=req.body;
+con.query("SELECT email FROM user WHERE password=? and username=?",[password,username],(error,result)=>{
+  if(error){
+    console.log(error);
+  }
+  else if(result.length==0){
+    return res.render("validation",{message:"Wrong information"})
+  }
+  else{
+    const email=result[0].email;
+    res.render("profUserChange",{username:username,password:password,email:email})
+  }
+})
+})
+router.post("/editedHomepage",(req,res)=>{
+  const {username,email,password,oldUsername,oldEmail,oldPassword}=req.body;
+
+   console.log(oldUsername);
+  con.query("UPDATE user SET username=?,email=?,password=? WHERE username=? and email=?",[username,email,password,oldUsername,oldEmail])
+  res.render("maps",{username:username})
+
+})
+
+router.post("/signedup",(req,res)=>{ 
+ const {username,email,password}=req.body;
+  con.query("SELECT email FROM user WHERE email=?",[email],(error,result)=>{
+    if(error){
+        console.log(error);
+    }
+ else if(result.length>0){
+   return res.render("signup",{message:"Email already exists"})
+  }
+  else{
+    res.render("login",{username:username})
+    con.query("INSERT INTO USER SET ?",{username:username,email:email,password:password,points:0,tokens:0})
+     }
+ })
+})
+
+router.get("/signup",(req,res)=>
+{
+    res.render("signup")   
+})
+
+
+router.post("/homepage",(req,res)=>{
+const {username,password}=req.body;
+con.query("SELECT username,password from user WHERE username=? and password=?",
+[username,password],(error,result)=>{
+    if(error){
+        console.log(error);
+    }
+    else if(result.length>0){
+
+return res.render("maps",{username:username})
+    }
+    else{
+        res.render("login",{message:"The account does not exist"})
+    }
+    })
+    })
+
+
+
+
+
+router.get("/logout",(req,res)=>{
+req.session.destroy();
+res.redirect("/")
+})
+router.post('/upload', async (req, res) => {
+    try {
+      if (!req.files) {
+        console.log("Failed");
+        res.send({
+          status: false,
+          message: 'No file uploaded'
+        })
+      } else {
+        // Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+        let jsonFile = req.files.file
+  
+        // Use the mv() method to place the file in the upload directory (i.e. "uploads")
+        jsonFile.mv('./uploads/' + jsonFile.name)
+  
+        //send response
+        res.send({
+          status: true,
+          message: 'File is uploaded',
+          data: {
+            name: jsonFile.name,
+            mimetype: jsonFile.mimetype,
+            size: jsonFile.size
+          }
+        });
+
+        //Read uploaded json
+        //updateSupermarkets(jsonFile.name);
+      }
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  })
+
+router.post("/database/update", async (req, res) => {
+  try{
+    if(req.body.type == "addSuper"){
+      updateSupermarkets(req.body.fileName)
+    }
+    if(req.body.type == "deleteSuper"){
+      removeSupermarkets(req.body.fileName)
+    }
+    if(req.body.type == "addProd"){
+      updateProducts(req.body.fileName)
+    }
+    if(req.body.type == "deleteProd"){
+      removeProducts(req.body.fileName)
+    }
+
+
+    res.send({
+      status: true,
+      message: 'Database updated'
+    });
+
+  }catch(err){
+    res.status(500).send(err)
+  }
+});
+
+router.post("/database/get", async (req, res) => {
+  try{
+    //Fetch supermarkets
+    if(req.body.type == "super"){
+      let formData = new FormData();
+
+      var data;
+
+      const query = "select * from store;";
+
+      con.query(query, (err, result) => {
+        if (err) {
+            console.error(err);
+        } else {
+          //Send the data to frontend
+          res.json({message: result});
+        }
+    });
+    }
+
+    //Fetch products
+    if(req.body.type == "prod"){
+
+    }
+  }
+  catch(err){
+    res.status(500).send(err)
+  }
+})
+
+export{router}
+
+
+
+
